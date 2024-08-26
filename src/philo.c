90d26389567ec0e3413ebb03a4c154ba14f64619
AgustinPaternoster <you@example.com>
2024-08-26 12:47:27 +0200

    arreglo

--- src/philo.c
+++ src/philo.c
@@ -20,22 +20,39 @@ void select_fork(t_philo *philo)
 		pthread_mutex_lock(&philo->right_fork->fork);
 		ft_print_state(philo,FORK,philo->right_fork->fork_nb);
 	}
-
 }
+
+void star_eating(t_philo *philo)
+{
+	select_fork(philo);
+	ft_print_state(philo,EAT,1);
+	pthread_mutex_lock(&philo->data->meals);
+	philo->last_meal = get_date_time();
+	// philo->meals++;
+	pthread_mutex_unlock(&philo->data->meals);
+	ft_usleep(philo->data->time_eat);
+	pthread_mutex_unlock(&philo->left_fork->fork);
+	pthread_mutex_unlock(&philo->right_fork->fork);
+}
+
 void *philo_rutine(void *philos)
 {
 	t_philo *philo;
-	
-	
+
+	int i = 0;
 	philo = (t_philo*)philos;
 	while(!get_safe_bool(&philo->data->syncro,&philo->data->all_ready));
-	// while(!dead_loop(philo)
+	// while(!dead_loop(philo))
 	// {
+		
 
 	// }
-	select_fork(philo);
-	ft_usleep(200);
-	pthread_mutex_unlock(&philo->left_fork->fork);
-	pthread_mutex_unlock(&philo->right_fork->fork);
+	while(i < 2)
+	{
+		star_eating(philo);
+		i++;
+	}
+
+	
 	return (NULL);
 }
